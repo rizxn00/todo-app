bd4da26147e6291be325d5e8de5ba9f7e7e54000
RizinGH <rizi.n2021@gmail.com>
2024-06-01 23:41:50 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -6,18 +6,18 @@ const cors = require("cors")
 
 const app = express()
 const PORT = process.env.PORT || 8000;
-const routes =require('./routers/routes')
-app.use (express. json ())
-app.use (cors ({
-    origin:['https://todo-app-frontend-indol-beta.vercel.app'],
-    methods:['GET','POST'],
-    credentials:true
-}))
+const routes = require('./routers/routes')
+app.use(express.json())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 
 mongoose
-.connect (process.env.MONGODB_URL)
-.then (()=> console. log( `Connected To MongoDB...`))
-.catch((err)=>console.log(err))
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log(`Connected To MongoDB...`))
+    .catch((err) => console.log(err))
 app.use(routes)
 app.listen(PORT, () => console.log(`Listening on: ${PORT}`));
 
