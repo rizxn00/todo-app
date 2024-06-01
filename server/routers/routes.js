e36bfa0cfc40eb528517b7d95ef506370ae7891c
RizinGH <rizi.n2021@gmail.com>
2024-06-01 22:42:28 +0530

    scripts change

--- server/routers/routes.js
+++ server/routers/routes.js
@@ -3,6 +3,9 @@ const router = Router()
 
 const {createtodo, gettodo, deletetodo, updatetodo} = require("../controllers/todo")
 
+router.get('/', (req, res) => {
+    return res.send("Success")
+})
 router.post('/todo', createtodo)
 router.get('/gettodo',gettodo)
 router.post('/deletetodo', deletetodo)
